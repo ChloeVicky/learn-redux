d6661f5d78f383983bb931a50c043b029a2247ba
ldl <919578094@qq.com>
2021-08-09 00:06:25 +0800

    first try

--- src/index.js
+++ src/index.js
@@ -4,39 +4,40 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 
+// First try
 // ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
 
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
 
 // REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type){
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
+// const counter = (state = 0, action) => {
+//   switch (action.type){
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
 
-}
+// }
 
-const store = createStore(counter);
+// const store = createStore(counter);
 
 // once the state change, store.subscribe() will execute immediately
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
 // DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
