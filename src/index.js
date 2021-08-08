9da6ef10194a97e75edce876b34b5b3fa6eef95c
ldl <919578094@qq.com>
2021-08-09 00:03:54 +0800

    create store, action, reducer, dispatch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type){
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+
+}
+
+const store = createStore(counter);
+
+// once the state change, store.subscribe() will execute immediately
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
