fe8fdf1eefde22735936db3040d70d48f839f930
ldl <919578094@qq.com>
2021-08-09 16:18:01 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        hello
-      </div>
-    )
-  }
+import React, { Component } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { increment,decrement } from './actions';
+function App () {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {/* <h3>isLogged: {isLogged.toString()}</h3> */}
+      {isLogged ? 
+        <h3>something that can not be seen if it is not logged.</h3>
+        :<h3>you are not logged in</h3>
+      }
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <h1>counter {counter}</h1>
+      <button onClick={() => dispatch(increment(10))}>+</button>
+    </div>
+  )
 }
-
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
-
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 // First try
 // ACTION INCREMENT
 // const increment = () => {
@@ -40,8 +45,8 @@ import { createStore } from 'redux';
 // store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
